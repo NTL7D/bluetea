3ce33dd9d39edbef6d3bea3e1bc1ab10da72859b
NTL7D <55150110+NTL7D@users.noreply.github.com>
2022-05-05 22:47:34 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
+import { HomeApp } from "./HomeApp";
 import { Login } from "./components/account/login/login";
 import { Register } from "./components/account/register/register";
 import { NavigationContainer } from "@react-navigation/native";
-import { HomeApp } from "./HomeApp";
 import { createStackNavigator } from "@react-navigation/stack";
-
 
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  const [email, setEmail] = useState(null);
-
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {email ? (
-          <Stack.Screen name="Home">
-            {(props) => <HomeApp {...props} extraData={email} />}
-          </Stack.Screen>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-          </>
-        )}
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+        />
+        <Stack.Screen name="Home" component={HomeApp} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
