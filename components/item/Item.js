f7bd447fe9672b8338a10ba695e1b0c0e85e7e4c
NTL7D <55150110+NTL7D@users.noreply.github.com>
2022-05-05 22:51:33 +0700

    Add files via upload

--- components/item/Item.js
+++ components/item/Item.js
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { CoffeeScreen } from '../../Screen/CoffeeScreen';
-import { BobaScreen } from '../../Screen/BobaScreen';
-import { AllItem } from './all';
+import React, { useState } from "react";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { CoffeeScreen } from "../../Screen/CoffeeScreen";
+import { BobaScreen } from "../../Screen/BobaScreen";
+import { AllItem } from "./all";
 
 const Tab = createMaterialTopTabNavigator();
 
 export const ItemList = ({ navigation }) => {
-
   return (
     <View>
-          <NavigationContainer independent={true}>
+      <NavigationContainer independent={true}>
         <Tab.Navigator>
           <Tab.Screen name="Tất cả" component={AllItem} />
           <Tab.Screen name="Cà phê" component={CoffeeScreen} />
