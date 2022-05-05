c15a24dea8e0892158d7c186b5dbcb38d224be19
NTL7D <55150110+NTL7D@users.noreply.github.com>
2022-05-05 22:43:34 +0700

    Update ProfileDetail.js

--- components/account/profile/ProfileDetail.js
+++ components/account/profile/ProfileDetail.js
@@ -1,17 +1,29 @@
-import React, {useState} from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from "../login/login"
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { auth } from "../../../firebase";
 export const ProfileDetail = ({ navigation }) => {
-      
-      const [order, setOrder] = useState([
+  const [order, setOrder] = useState([
     {
-      name: 'Cà phê đá',
+      name: "Cà phê đá",
       price: 10000,
     },
-
   ]);
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <View style={styles.profile}>
@@ -19,21 +31,20 @@ export const ProfileDetail = ({ navigation }) => {
         <Image />
       </View>
       <View style={styles.text}>
-        <Text style={styles.name}>John Doe</Text>
+        <Text style={styles.name}>{auth.currentUser?.email}</Text>
         <Text style={styles.detail}>1996</Text>
         <Text style={styles.detailOrder}>Number of order: 1</Text>
       </View>
       <View style={styles.button}>
-        <TouchableOpacity
-          style={styles.logout}
-          onPress={() => {()=>navigation.goBack()}} >
+        <TouchableOpacity style={styles.logout} onPress={handleSignOut}>
           <Text
             style={{
-              color: '#fff',
-              fontWeight: 'bold',
+              color: "#fff",
+              fontWeight: "bold",
               fontSize: 18,
               marginTop: 2,
-            }}>
+            }}
+          >
             Đăng xuất
           </Text>
         </TouchableOpacity>
@@ -44,59 +55,59 @@ export const ProfileDetail = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   profile: {
-    backgroundColor: '#A7C7E7',
-    width: '90%',
+    backgroundColor: "#A7C7E7",
+    width: "90%",
     height: 250,
     borderRadius: 10,
   },
 
   avatar: {
-    position: 'absolute',
+    position: "absolute",
     top: -80,
-    left: 80,
+    left: 100,
     width: 150,
     height: 150,
-    borderRadius: '50%',
-    backgroundColor: '#000',
+    borderRadius: "50%",
+    backgroundColor: "#000",
   },
 
   text: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 70,
   },
 
   name: {
     fontSize: 30,
-    fontWeight: 'bold',
-    fontFamily: 'Times',
+    fontWeight: "bold",
+    fontFamily: "Times",
   },
 
   detail: {
     fontSize: 15,
     fontWeight: 400,
-    opacity: '40%',
+    opacity: "40%",
     marginTop: 15,
   },
 
   detailOrder: {
     fontSize: 15,
     fontWeight: 400,
-    opacity: '40%',
+    opacity: "40%",
     marginTop: 10,
   },
 
   button: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   logout: {
-    backgroundColor: '#1F618D',
-    width: '120px',
-    height: '40px',
+    backgroundColor: "#1F618D",
+    width: "120px",
+    height: "40px",
     padding: 5,
-    textAlign: 'center',
+    textAlign: "center",
     borderRadius: 5,
   },
 });
